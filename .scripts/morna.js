3f9dd1d6270ea262de927d9d26435b4af126a780
lagleki <gleki.is.my.name@gmail.com>
2017-06-11 05:18:25 -0700

    commit

--- .scripts/morna.js
+++ .scripts/morna.js
@@ -14,7 +14,7 @@ function addZero(i) {
 //strip out template
 function gp(file_m,tag,prefix){
 	const m = (file_m.match(new RegExp("^ *"+(prefix===0?'':"window\.") + tag +" *= *[\"']?(.*?)[\"']?;(\\\n|\\\r)",""))||['','false'])[1].replace(/\\\"/g,"\"");
-	console.log(tag,m);
+	if tag.indexOf("title") console.log(tag,m);
 	if (m=='true') return true;
 	if (m=='false') return undefined;
 	return m;
